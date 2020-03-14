d91cbbed4373b71f47711f236ae26d17899e120e
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-14 15:16:48 -0300

    Configurar o Swagger e o SwaggerUi

--- src/CellCms.Api/Startup.cs
+++ src/CellCms.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CellCms.Api
 {
@@ -25,6 +26,18 @@ namespace CellCms.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adicionando a geração do swagger.json
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Cell CMS",
+                    Description = "API para o Cell CMS",
+                    Version = "v1",
+                    Contact = new OpenApiContact { Name = "Rodolpho Alves", Url = new Uri("https://github.com/rodolphocastro/cell-cms") }
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -36,6 +49,9 @@ namespace CellCms.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            // Usando o Middleware para expor o swagger.json
+            app.UseSwagger();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -45,6 +61,13 @@ namespace CellCms.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Usando o Middleware para export o SwaggerUi
+            app.UseSwaggerUI(cfg =>
+            {
+                cfg.RoutePrefix = string.Empty;
+                cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "Cell CMS API");
             });
         }
     }
