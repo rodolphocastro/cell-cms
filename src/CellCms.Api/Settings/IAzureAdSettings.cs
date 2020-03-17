772264dcb816ad7345208e16bbff8a147c15ab24
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-16 22:23:47 -0300

    Definir parametros para o AzureAd

---
+++ src/CellCms.Api/Settings/IAzureAdSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace CellCms.Api.Settings
+{
+    /// <summary>
+    /// Descreve as propriedades para configurar autenticação através do Azure AD.
+    /// </summary>
+    public interface IAzureAdSettings
+    {
+        /// <summary>
+        /// ClientId esperado.
+        /// </summary>
+        string ClientId { get; }
+
+        /// <summary>
+        /// Endpoint para obter access tokens.
+        /// </summary>
+        string TokenEndpoint { get; }
+
+        /// <summary>
+        /// Endpoint para obter autorização dos usuários.
+        /// </summary>
+        string AuthorizeEndpoint { get; }
+
+        /// <summary>
+        /// Endpoint para buscar o well-known document.
+        /// </summary>
+        string MetadataEndpoint { get; }
+    }
+}
