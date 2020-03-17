3ef694f3a3bc6440d96b40d99903473444fd22d2
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-16 22:23:57 -0300

    Adicionar configurações do AzureAd

---
+++ src/CellCms.Api/Settings/AzureAdSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace CellCms.Api.Settings
+{
+    /// <summary>
+    /// Configurações do Azure AD.
+    /// </summary>
+    public class AzureAdSettings : IAzureAdSettings
+    {
+        /// <summary>
+        /// Chave para buscar as configurações.
+        /// </summary>
+        public const string SettingsKey = "AzureAd";
+
+        public string ClientId { get; set; }
+
+        public string TokenEndpoint { get; set; }
+
+        public string AuthorizeEndpoint { get; set; }
+
+        public string MetadataEndpoint { get; set; }
+    }
+}
