770d5b55a9ac8fa7911091e1af342c8df55abfdf
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-17 22:46:41 -0300

    Adicionar models da Aplicação

---
+++ src/CellCms.Api/Models/Feed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CellCms.Api.Models
+{
+    /// <summary>
+    /// Feed para conteúdos do Cell CMS.
+    /// </summary>
+    public class Feed
+    {
+        /// <summary>
+        /// Chave primária do Feed.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do Feed.
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Tags pertencentes ao Feed
+        /// </summary>
+        public ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
+    }
+}
---
+++ src/CellCms.Api/Models/Tag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace CellCms.Api.Models
+{
+    /// <summary>
+    /// Tag de um Feed.
+    /// </summary>
+    public class Tag
+    {
+        /// <summary>
+        /// Chave primária.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Feed ao qual a Tag pertence.
+        /// </summary>
+        public Feed Feed { get; set; }
+
+        /// <summary>
+        /// Chave Primária do Feed.
+        /// </summary>
+        public int FeedId { get; set; }
+
+        /// <summary>
+        /// Nome da Tag.
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Conteúdos relacionados à tag.
+        /// </summary>
+        public ICollection<ContentTag> ContentTags { get; set; }
+    }
+}
