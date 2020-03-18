7a18e44b35332184027748b1b46197074c9f92ff
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-17 21:23:02 -0300

    Melhorar recuperação do AzureAdSettings

---
+++ src/CellCms.Api/Settings/Extensions/CellConfigurationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using CellCms.Api.Settings;
+using System;
+
+namespace Microsoft.Extensions.Configuration
+{
+    /// <summary>
+    /// Extensions para facilitar a busca de configurações do Cell CMS.
+    /// </summary>
+    public static class CellConfigurationExtensions
+    {
+        /// <summary>
+        /// Recupera as configurações do Azure Active Directory.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns>ao settings encontrados</returns>
+        public static IAzureAdSettings GetAzureAdSettings(this IConfiguration configuration)
+        {
+            if (configuration is null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            var azureAdSettings = configuration.GetSection(AzureAdSettings.SettingsKey).Get<AzureAdSettings>();
+            if (azureAdSettings is null)
+            {
+                throw new InvalidOperationException("As configurações do AzureAD são necessárias para a API");
+            }
+
+            return azureAdSettings;
+        }
+    }
+}
