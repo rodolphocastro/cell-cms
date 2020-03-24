88e9e2f4c6abf8c1fe36a70944b0d702219ebd10
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-23 22:43:03 -0300

    Extrair context para a aplicação

---
+++ src/CellCms.Api/CellContext.cs
@@ -0,0 +1,49 @@
+﻿using CellCms.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CellCms.Api
+{
+    /// <summary>
+    /// Context para a API do CellCMS.
+    /// </summary>
+    public class CellContext : DbContext
+    {
+        /// <summary>
+        /// Chave para buscar a ConnectionString do Context.
+        /// </summary>
+        public const string ConnectionStringKey = "CellCmsContext";
+
+        /// <summary>
+        /// Novo context para o Cell CMS.
+        /// </summary>
+        /// <param name="options">Opções a serem definidas para o Context</param>
+        public CellContext(DbContextOptions options) : base(options) { }
+
+        /// <summary>
+        /// Tabela de Feeds.
+        /// </summary>
+        public DbSet<Feed> Feeds { get; protected set; }
+
+        /// <summary>
+        /// Tabela de Tags.
+        /// </summary>
+        public DbSet<Tag> Tags { get; protected set; }
+
+        /// <summary>
+        /// Tabela de Contents.
+        /// </summary>
+        public DbSet<Content> Contents { get; protected set; }
+
+        /// <summary>
+        /// Configuração da criação dos Models.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+    }
+}
