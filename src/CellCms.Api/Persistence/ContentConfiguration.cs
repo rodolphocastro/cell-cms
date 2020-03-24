2a54211a9ff814a9d201b50ab3c12b452daf824d
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-23 22:27:16 -0300

    Adicionar configuration das entidades

---
+++ src/CellCms.Api/Persistence/ContentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using CellCms.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CellCms.Api.Persistence
+{
+    /// <summary>
+    /// Configurações para Contents.
+    /// </summary>
+    public class ContentConfiguration : IEntityTypeConfiguration<Content>
+    {
+        public void Configure(EntityTypeBuilder<Content> builder)
+        {
+            // Tabela
+            builder.ToTable("Contents");
+
+            // Chave
+            builder.HasKey(c => c.Id);
+
+            // Propriedades
+            builder.Property(c => c.DthCriacao);
+
+            builder
+                .Property(c => c.Titulo)
+                .HasMaxLength(255)
+                .IsRequired(true);
+
+            builder
+                .Property(c => c.Corpo)
+                .HasMaxLength(3000)
+                .IsRequired(true);
+
+            // Importante: Os relacionamentos já foram definidos nos outros arquivos!
+        }
+    }
+}
---
+++ src/CellCms.Api/Persistence/TagConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using CellCms.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CellCms.Api.Persistence
+{
+    /// <summary>
+    /// Configurações para Tags.
+    /// </summary>
+    public class TagConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            // Definindo a Tabela
+            builder.ToTable("Tags");
+
+            // Chave Primária
+            builder.HasKey(t => t.Id);
+
+            // Propriedades
+            builder
+                .Property(t => t.Nome)
+                .HasMaxLength(250)
+                .IsRequired(true);
+
+            // Relacionamentos
+            // Importante: Note que o relacionamento com Feed foi configurado pelo FeedConfiguration
+            // e o Relacionamento entre Tags e Content está no ContentTag configuration
+            // então aqui não precisamos fazer nada ;)
+
+        }
+    }
+}
