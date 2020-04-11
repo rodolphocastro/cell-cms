34988dad13844f50958bb271d6dbbf1fbc627630
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-10 21:28:48 -0300

    Configurar o Serilog

--- src/CellCms.Api/Program.cs
+++ src/CellCms.Api/Program.cs
@@ -1,5 +1,10 @@
+using System;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+
+using Serilog;
+using Serilog.Formatting.Compact;
 
 namespace CellCms.Api
 {
@@ -7,14 +12,47 @@ namespace CellCms.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args)
+            // Criando a instância do Logger que utilizaremos na API
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Debug()       // Caso estejamos em Debug, capturar estes logs
+#else
+                .MinimumLevel.Information() // Senão, apenas informações
+#endif
+                // Indicando que queremos mensagens Information que vierem do Namespace Microsoft
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+                // Indicando que queremos mensagens Warning ou maiores que vierem do Namespace Microsoft.EntityFrameworkCore
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Warning)
+                .Enrich.FromLogContext()    // Indicando que é pra utilizar enriquecer os logs automaticamente com o contexto de execução
+                .WriteTo.Console()          // Indicando que os logs devem ser apresentados no console
+                .WriteTo.File(new CompactJsonFormatter(), "logs/log.txt", rollingInterval: RollingInterval.Day) // Indicando que os logs devem ser salvos em um txt para cada dia
+#if DEBUG
+                .WriteTo.Debug()    // Indicando que os logs devem ser apresentados no Debug Console
+#endif
+                .CreateLogger();    // Criando o logger, finalmente
+
+            // Como agora temos log antes da execução, podemos encapsular a execução com um Try-Catch para logar erros críticos
+            try
+            {
+                CreateHostBuilder(args)
                 .Build()
                 .EnsureDatabaseIsMigrated()
                 .Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Erro ao iniciar a API");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
