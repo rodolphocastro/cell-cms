3ddc7fad589302b9731f093f7653c5297262acaa
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-05-28 22:36:26 -0300

    tests: Create tests for the API plumbing

---
+++ tests/CellCms.Tests.Unit/Configuration/ConfigurationExtensionsTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CellCms.Api.Settings;
+using CellCms.Tests.Unit.Utils;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Configuration;
+
+using Xunit;
+
+namespace CellCms.Tests.Unit.Configuration
+{
+    [Trait(TraitsConstants.Category.Name, TraitsConstants.Category.Values.Unit)]
+    [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Plumbing)]
+    public partial class ConfigurationExtensionsTests
+    {
+        public class AzureAdSetingsTests
+        {
+            [Theory, CreateData]
+            public void GetAzureAdSettings_Valid_Returns(AzureAdSettings expected)
+            {
+                // Arrange                                                
+                var subject = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new[] {
+                        KeyValuePair.Create("AzureAd:AuthorizeEndpoint", expected.AuthorizeEndpoint),
+                        KeyValuePair.Create("AzureAd:ClientId", expected.ClientId),
+                        KeyValuePair.Create("AzureAd:MetadataEndpoint", expected.MetadataEndpoint),
+                        KeyValuePair.Create("AzureAd:TokenEndpoint", expected.TokenEndpoint)
+                    })
+                    .Build();
+                // Act
+                var result = subject.GetAzureAdSettings();
+
+                // Assert
+                result.Should().NotBeNull();
+                result.Should().BeEquivalentTo(expected);
+            }
+
+            [Fact]
+            public void GetAzureAdSettings_Invalid_ThrowsInvalidOperation()
+            {
+                // Arrange                                                
+                var subject = new ConfigurationBuilder()
+                    .AddInMemoryCollection()
+                    .Build();
+                // Act
+
+                Action act = () => subject.GetAzureAdSettings();
+
+                // Assert
+                act.Should().ThrowExactly<InvalidOperationException>();
+            }
+
+            [Fact]
+            public void GetAzureAdSettings_Null_hrowsArgumentNull()
+            {
+                // Arrange
+                IConfiguration subject = null;
+
+                // Act
+                Action act = () => subject.GetAzureAdSettings();
+
+                // Assert
+                act.Should().ThrowExactly<ArgumentNullException>();
+            }
+        }
+    }
+}
---
+++ tests/CellCms.Tests.Unit/Swagger/SwaggerExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CellCms.Api.Settings;
+using CellCms.Tests.Unit.Utils;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Xunit;
+
+namespace CellCms.Tests.Unit.Swagger
+{
+    [Trait(TraitsConstants.Category.Name, TraitsConstants.Category.Values.Unit)]
+    [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Plumbing)]
+    public partial class SwaggerExtensionsTests
+    {
+        public class SwaggerServicesExtensionsTests
+        {
+            [Theory, CreateData]
+            public void AddCellSwagger_NullCollection_ThrowsArgumentNullException(
+                IConfiguration config)
+            {
+                // Arrange
+                IServiceCollection subject = null;
+
+                // Act
+                Action act = () => subject.AddCellSwagger(config);
+
+                // Assert
+                act.Should().ThrowExactly<ArgumentNullException>();
+            }
+
+            [Theory, CreateData]
+            public void AddCellSwagger_NullConfiguration_ThrowsArgumentNullException(
+                IServiceCollection subject)
+            {
+                // Arrange
+
+                // Act
+                Action act = () => subject.AddCellSwagger(null);
+
+                // Assert
+                act.Should().ThrowExactly<ArgumentNullException>();
+            }
+
+            [Theory, CreateData]
+            public void AddCellSwagger_Valid_Returns(
+                AzureAdSettings settings,
+                IServiceCollection subject)
+            {
+                // Arrange                
+                var config = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { "AzureAd:AuthorizeEndpoint", settings.AuthorizeEndpoint },
+                        { "AzureAd:ClientId", settings.ClientId },
+                        { "AzureAd:MetadataEndpoint", settings.MetadataEndpoint },
+                        { "AzureAd:TokenEndpoint", settings.TokenEndpoint }
+                    })
+                    .Build();
+
+                // Act
+                var result = subject.AddCellSwagger(config);
+
+                // Assert
+                result.Should().NotBeNull();
+            }
+        }
+    }
+}
