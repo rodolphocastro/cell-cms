f24aec3c359254f16eda3214722e996e4bc460c7
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-06-09 18:39:22 -0300

    chore: Setup APIClient class

---
+++ src/CellCms.Api/Auth/ApiClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace CellCms.Api.Auth
+{
+    /// <summary>
+    /// Um cliente autorizado a chamar a API.
+    /// </summary>
+    public record ApiClient(string Name, string Key) : IEquatable<string>
+    {
+        public virtual bool Equals(string other) => Key.Equals(other, StringComparison.InvariantCulture);
+    }
+
+    /// <summary>
+    /// Queries para consultar ApiClients.
+    /// </summary>
+    public static class ApiClientQueries
+    {
+        /// <summary>
+        /// Filtra clients baseados em uma key.
+        /// </summary>
+        /// <param name="clients"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static IQueryable<ApiClient> WithKey(this IQueryable<ApiClient> clients, string key) => clients.Where(c => c.Equals(key));
+
+        /// <summary>
+        /// Query para verificar se existe um client para uma key específica.
+        /// </summary>
+        /// <param name="clients"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool IsAuthorized(this IQueryable<ApiClient> clients, string key) => clients.WithKey(key).Any();
+    }
+}
---
+++ tests/CellCms.Tests.Unit/Auth/ApiKeyTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using CellCms.Api.Auth;
+using CellCms.Tests.Unit.Utils;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace CellCms.Tests.Unit.Auth
+{
+    [Trait(TraitsConstants.Category.Name, TraitsConstants.Category.Values.Unit)]
+    public class ApiKeyTests
+    {
+        [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Domain)]
+        public class ApiClientQueriesTests
+        {
+            private readonly IEnumerable<ApiClient> _empty;
+
+            public ApiClientQueriesTests()
+            {
+                _empty = Enumerable.Empty<ApiClient>();
+            }
+
+            [Theory, CreateData]
+            public void WithKey_Contains_Returns(IEnumerable<ApiClient> clients)
+            {
+                // Arrange
+                var subject = clients.ToList().AsQueryable();
+
+                // Act
+                var result = subject.WithKey(subject.First().Key);
+
+                // Assert
+                result.Should().NotBeNullOrEmpty();
+            }
+
+            [Theory, CreateData]
+            public void WithKey_DoesntContain_ReturnsEmpty(string fakeKey)
+            {
+                // Arrange
+                var subject = _empty.ToList().AsQueryable();
+
+                // Act
+                var result = subject.WithKey(fakeKey);
+
+                // Assert
+                result.Should().BeEmpty();
+            }
+
+            [Theory, CreateData]
+            public void IsAuthorized_Contains_ReturnsTrue(IEnumerable<ApiClient> clients)
+            {
+                // Arrange
+                var subject = clients.ToList().AsQueryable();
+                var expected = subject.First();
+
+                // Act
+                var result = subject.IsAuthorized(expected.Key);
+
+                // Assert
+                result.Should().BeTrue();
+            }
+
+            [Theory, CreateData]
+            public void IsAuthorized_DoesntContain_ReturnsFalse(string fakeKey)
+            {
+                // Arrange
+                var subject = _empty.ToList().AsQueryable();
+
+                // Act
+                var result = subject.IsAuthorized(fakeKey);
+
+                // Assert
+                result.Should().BeFalse();
+            }
+        }
+
+        [Trait(TraitsConstants.Label.Name, TraitsConstants.Label.Values.Domain)]
+        public class ApiClientTests
+        {
+            [Theory, CreateData]
+            public void Equals_String_MatchesKey(ApiClient subject)
+            {
+                // Arrange
+                var key = subject.Key;
+
+                // Act
+                var result = subject.Equals(key);
+
+                // Assert
+                result.Should().BeTrue();
+            }
+        }
+    }
+}
